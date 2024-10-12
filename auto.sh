3736880fd13bb3bae567ad643f7142cc0ad8d2e8
Suzu <106862353+dogliu666@users.noreply.github.com>
2024-10-12 18:21:47 +0800

    Add files via upload

--- auto.sh
+++ auto.sh
@@ -1,15 +1,32 @@
+#!/bin/bash
+
+wget https://github.com/dogliu666/ESurfingDialer-For-Docker/releases/download/Latest/Dialer.zip
+
+# 检查文件是否存在
+if [ ! -f "Dialer.zip" ]; then
+    echo "Dialer.zip 文件不存在"
+    exit 1
+fi
+
+# 检查文件大小是否合理
+if [ $(stat -c%s "Dialer.zip") -le 100 ]; then
+    echo "下载的 Dialer.zip 文件大小异常"
+    exit 1
+fi
+
+# 检查文件是否为有效的 zip 文件
 if ! unzip -tq Dialer.zip; then
     echo "Dialer.zip 文件无效或已损坏"
     exit 1
 fi
 
 # 解压缩 Dialer.zip 文件
-unzip -o Dialer.zip -d /root/dialer
+unzip -o Dialer.zip -d /root/Dialer
 if [ $? -ne 0 ]; then
     echo "解压缩 Dialer.zip 失败"
     exit 1
 fi
-cd /root/dialer || { echo "目录 /root/Dialer 不存在"; exit 1; }
+cd /root/Dialer || { echo "目录 /root/Dialer 不存在"; exit 1; }
 
 # 构建 Docker 容器
 docker build -t dialer .
@@ -19,14 +36,14 @@ if [ $? -ne 0 ]; then
 fi
 
 # 导出 Docker 镜像
-docker save -o dialer.tar dialer
+docker save -o Dialer.tar dialer
 if [ $? -ne 0 ]; then
     echo "Docker 镜像导出失败"
     exit 1
 fi
 
 # 加载 Docker 镜像
-docker load -i ./dialer.tar
+docker load -i ./Dialer.tar
 if [ $? -ne 0 ]; then
     echo "Docker 镜像加载失败"
     exit 1
@@ -51,7 +68,7 @@ echo "pwd=$pwd" >> Config.txt
 source ./Config.txt
 
 # 运行 docker run 命令
-docker run -itd -e DIALER_USER="$account" -e DIALER_PASSWORD="$pwd" --name dialer-client --network host --restart=always dialer
+docker run -itd -e DIALER_USER="$account" -e DIALER_PASSWORD="$pwd" --name dialer-client --network host --restart=always Dialer
 if [ $? -ne 0 ]; then
     echo "Docker 容器运行失败"
     exit 1
@@ -59,8 +76,7 @@ fi
 
 # 自动删除文件
 cd ..
-rm auto.sh
+rm -rf /root/dialer
 rm -f Dialer.zip
 rm -f dialer.tar
-
-echo "所有临时文件已删除"
+rm -f Config.txt
