124a6506de8204ba74a4db04bf5734688edbc2ac
Suzu <106862353+dogliu666@users.noreply.github.com>
2024-11-06 00:23:02 +0800

    添加 检测Docker、自动删除文件、自动检测Docker容器是否运行

--- auto.sh
+++ auto.sh
@@ -1,22 +1,40 @@
 #!/bin/bash
 cd /root
 
-wget https://github.com/dogliu666/ESurfingDialer-For-Docker/releases/download/Latest/Dialer.zip
-
-# 检查文件是否存在
-if [ ! -f "Dialer.zip" ]; then
-    echo "Dialer.zip 文件不存在"
+# 检查是否安装了 Docker
+if ! command -v docker &> /dev/null; then
+    echo "Docker 未安装"
     exit 1
+fi
+
+# 检查Dialer.zip文件是否存在
+FILE="Dialer.zip"
+
+if [ -f "$FILE" ]; then
+    read -p "文件 $FILE 已存在。是否删除并重新下载？(y/n): " choice
+    case "$choice" in 
+        y|Y ) 
+            rm -f auto.sh
+            rm -rf /root/Dialer
+            rm -f /root/Dialer.zip
+            rm -f dialer.tar
+            echo "下载 Dialer.zip 文件"
+            wget https://github.com/dogliu666/ESurfingDialer-For-Docker/releases/download/Latest/Dialer.zip
+            ;;
+        n|N ) 
+            echo "跳过下载"
+            ;;
+        * ) 
+            echo "无效选择，跳过下载"
+            ;;
+    esac
+else
+    echo "下载 Dialer.zip 文件"
+    wget https://github.com/dogliu666/ESurfingDialer-For-Docker/releases/download/Latest/Dialer.zip
 fi
 
 # 解压缩 Dialer.zip 文件
-if ! unzip -tq Dialer.zip; then
-    echo "解压缩 Dialer.zip 失败"
-    exit 1
-fi
-
-unzip -o Dialer.zip -d /root/Dialer
-if [ $? -ne 0 ]; then
+if ! unzip -o Dialer.zip -d /root/Dialer; then
     echo "解压缩 Dialer.zip 失败"
     exit 1
 fi
@@ -48,6 +66,8 @@ echo "Docker 镜像加载成功"
 
 # 输入 校园网 账密 并 保存到 Config.txt 文件
 read -p "请输入账号和密码（用空格分隔）: " account pwd
+echo "account=$account" > Config.txt
+echo "pwd=$pwd" >> Config.txt
 
 # 验证输入的账号和密码
 while [ -z "$account" ] || [ -z "$pwd" ]; do
@@ -55,14 +75,25 @@ while [ -z "$account" ] || [ -z "$pwd" ]; do
     read -p "请输入账号和密码（用空格分隔）: " account pwd
 done
 
-# 保存到 Config.txt 文件
-echo "account=$account" > Config.txt
-echo "pwd=$pwd" >> Config.txt
-
 # 从 Config.txt 文件中读取 account 和 pwd
 . /root/Dialer/Config.txt
 
 # 运行 docker run 命令
 docker run -itd -e DIALER_USER="$account" -e DIALER_PASSWORD="$pwd" --name dialer-client --network host --restart=always dialer
 
-echo "Docker 容器运行成功"
+sleep 5
+
+# 检查 Docker 容器是否正在运行
+if docker ps | grep -q dialer-client; then
+    echo "Docker 容器正在运行"
+else
+    echo "Docker 容器未运行"
+    echo "5s 后再次尝试运行 Docker 容器"
+    sleep 5
+    if docker ps | grep -q dialer-client; then
+        echo "Docker 容器正在运行"
+    else
+        echo "Docker 容器未运行，请检查"
+        exit 1
+    fi
+fi
